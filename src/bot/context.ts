5de1258d21242ee02fbdcb411f61775ba85ba334
shaunnope <seanyap123@gmail.com>
2023-08-14 11:58:11 +0800

    feat: case-insensitive parsing of bot commands

--- src/bot/context.ts
+++ src/bot/context.ts
@@ -1,6 +1,6 @@
 import { Update, UserFromGetMe } from "@grammyjs/types";
 import { UserPayload } from "@prisma/client";
-import { Context as DefaultContext, SessionFlavor, type Api } from "grammy";
+import { Context as DefaultContext, SessionFlavor, type Api, type CommandContext } from "grammy";
 import type { Container } from "~/container";
 import { Logger } from "~/logger";
 
@@ -45,15 +45,69 @@ export type Context = ParseModeFlavor<
   >
 >;
 
+// HACK: Override the implementation of `ctx.has.command` for case-insensitive handling of bot commands
+type MaybeArray<T> = T | T[]
+type StringWithSuggestions<S extends string> = (string & Record<never, never>) | S;
+
+function toArray(e: any) {
+  return Array.isArray(e) ? e : [e];
+}
+
+const customChecker = DefaultContext.has;
+customChecker.command = function<S extends string>(command: MaybeArray<StringWithSuggestions<S | "start" | "help" | "settings">>): <C extends DefaultContext>(ctx: C) => ctx is CommandContext<C> {
+  const hasEntities = customChecker.filterQuery(":entities:bot_command");
+  const atCommands = new Set();
+  const noAtCommands = new Set();
+  toArray(command).forEach((cmd) => {
+      if (cmd.startsWith("/")) {
+          throw new Error(`Do not include '/' when registering command handlers (use '${cmd.substring(1)}' not '${cmd}')`);
+      }
+      const set = cmd.indexOf("@") === -1 ? noAtCommands : atCommands;
+      set.add(cmd);
+  });
+  return (ctx) => {
+      var _a, _b;
+      if (!hasEntities(ctx))
+          return false;
+      const msg = (_a = ctx.message) !== null && _a !== void 0 ? _a : ctx.channelPost;
+      if (!msg) return false;
+      const txt = (_b = msg.text) !== null && _b !== void 0 ? _b : msg.caption;
+      if (!txt) return false;
+      return msg.entities.some((e) => {
+          if (e.type !== "bot_command")
+              return false;
+          if (e.offset !== 0)
+              return false;
+          const cmd = txt.substring(1, e.length);
+          if (noAtCommands.has(cmd) || atCommands.has(cmd)) {
+              ctx.match = txt.substring(cmd.length + 1).trimStart();
+              return true;
+          }
+          const index = cmd.indexOf("@");
+          if (index === -1)
+              return false;
+          const atTarget = cmd.substring(index + 1);
+          if (atTarget.toLowerCase() !== ctx.me.username.toLowerCase())
+              return false;
+          const atCommand = cmd.substring(0, index);
+          if (noAtCommands.has(atCommand)) {
+              ctx.match = txt.substring(cmd.length + 1).trimStart();
+              return true;
+          }
+          return false;
+      });
+  };
+};
+// END HACK
+
 export function createContextConstructor(container: Container) {
   return class extends DefaultContext implements ExtendedContextFlavor {
     container: Container;
-
     prisma: PrismaClientX;
-
     logger: Logger;
-
     scope: ContextScope;
+
+    static has = customChecker; // HACK: Override `ctx.has.command`
 
     constructor(update: Update, api: Api, me: UserFromGetMe) {
       super(update, api, me);
