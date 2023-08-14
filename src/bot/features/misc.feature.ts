a0d5375b087dc4f9e772d1dcb62f78d98fee07fc
shaunnope <seanyap123@gmail.com>
2023-08-14 12:06:51 +0800

    feat: add ping command

--- src/bot/features/misc.feature.ts
+++ src/bot/features/misc.feature.ts
@@ -10,4 +10,15 @@ feature.command('roll', logHandle('command-rng'), async ctx => {
   ctx.replyWithDice(['🎲', '🎯', '🎳', '🎰', '🏀', '⚽'][Math.floor(Math.random() * 6)])
 })
 
+feature.command('ping', logHandle('command-ping'), async ctx => {
+  const start = Date.now()
+  let ts = start - ctx.msg.date * 1000
+  let message = ctx.t('ping_command.ping', { ts: ts})
+  const msg = await ctx.reply(message)
+
+  ts = Date.now() - start
+  message += '\n' + ctx.t('ping_command.pong', { ts: ts })
+  await msg.editText(message)
+})
+
 export { composer as miscFeature }
