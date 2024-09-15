77e4731ff0fc2191649205260db40c7c1a23abb5
Sean Yap <seanyap123@gmail.com>
2024-09-15 20:24:28 +0800

    feat: varied player names

--- src/game/helpers/create-players.ts
+++ src/game/helpers/create-players.ts
@@ -1,9 +1,42 @@
+import _ from "lodash"
 import { Player } from "~/game/models/player"
 
+const PLAYERS = [
+  "Alice",
+  "Bianca",
+  "Cavin",
+  "Destin",
+  "Eugene",
+  "Franz",
+  "Gerry",
+  "Henry",
+  "Irene",
+  "Jonah",
+  "Keane",
+  "Louis",
+  "Moses",
+  "Nicole",
+  "Oscar",
+  "Peter",
+  "Quinn",
+  "Robin",
+  "Shane",
+  "Trent",
+  "Ursula",
+  "Victor",
+  "Wendy",
+  "Xavier",
+  "Yvonne",
+  "Zander",
+]
+
 export function createPlayers(count: number) {
+  count = _.clamp(count, 1, 10)
+  const names = _.sampleSize(PLAYERS, count)
+
   const players = new Array<Player>(count)
   for (let i = 0; i < count; i++) {
-    players[i] = new Player(i, `Player ${i}`)
+    players[i] = new Player(i, names[i])
   }
   return players
 }
